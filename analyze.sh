b4441e7ce68d56d683964a3a900112445fb34ac7
Aqamana06 <namako-kabe@keio.jp>
2024-05-26 19:12:22 +0900

    fix source tree

--- analyze.sh
+++ analyze.sh
@@ -1,37 +1,37 @@
 python analyzer/integrater.py \
     --dataset_dir_path dataset/activity \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name activity \
     --delete_first_column
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/calender \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name calender
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/cloud \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name cloud
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/heart \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name heart
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/POMS \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name POMS \
     --delete_first_column
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/Sleep \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name sleep \
     --delete_first_column
 
 python analyzer/integrater.py \
     --dataset_dir_path dataset/weather \
-    --output_dir_path outputs/ \
+    --output_dir_path csv/ \
     --dataset_name weather
